745d12096fdee8a83a611491ad0a4fa3ada16fe1
DaNoN1ck <125899397+DaNoN1ck@users.noreply.github.com>
2023-12-09 10:57:56 +0600

    Add files via upload

---
+++ Hotel/Hotel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Hotel
+{
+    public class Hotel
+    {
+        public uint RoomCount { get; set; }
+        public uint OccupiedRooms { get; set; }
+
+
+
+        public Hotel(uint initialRoomCount)
+        {
+            if (initialRoomCount < 1 || initialRoomCount > 1000)
+            {
+                throw new ArgumentException("Комнат должно быть от 1 до 1000");
+            }
+
+            RoomCount = initialRoomCount;
+        }
+
+        public void CheckIn()
+        {
+            if (OccupiedRooms < RoomCount)
+            {
+                OccupiedRooms++;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Нет свободных комнат");
+            }
+        }
+
+        public void CheckOut()
+        {
+            if (OccupiedRooms > 0)
+            {
+                OccupiedRooms--;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Нет комнат для выселения");
+            }
+        }
+
+
+    }
+}
